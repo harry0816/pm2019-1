d121a9c74b66788436b018c1b8b398d7bdb42e40
HyungJoo Park <harry0816@outlook.kr>
2019-04-03 11:51:15 +0900

    fix2

--- practice.cpp
+++ practice.cpp
@@ -6,6 +6,6 @@ int main() {
 	/*
 	*  TODO: replace "XXXX-XXXXX" to your student number
 	*/
-	cout << "My student number is 2018" << endl;
+	cout << "My student number is 2018-14041" << endl;
 	return 0
 }
